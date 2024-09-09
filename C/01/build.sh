a0a9553978126d1296c47d6f5fd9fe6335433dc3
AntonioBerna <asukeasukai12@gmail.com>
2024-09-09 21:15:45 +0200

    edited build file :fire:

--- C/01/build.sh
+++ C/01/build.sh
@@ -9,8 +9,16 @@ TARGET_OPTIONS="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20"
 
 mkdir -p $BINARY
 
-if [ "$1" = "-c" ]; then
-    rm -r $BINARY $LOG
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 [option]"
+    echo "Options:"
+    echo "  -c    Clean the project"
+    echo "  -b    Build the project"
+    echo "  -v    Test the project with Valgrind"
+    exit 1
+elif [ "$1" = "-c" ]; then
+    [ -d $BINARY ] && rm -r $BINARY
+    [ -d $LOG ] && rm -r $LOG
     echo "Clean completed."
     exit 0
 elif [ "$1" = "-b" ]; then
@@ -23,11 +31,4 @@ elif [ "$1" = "-v" ]; then
     valgrind $VALGRIND_FLAGS $BINARY/$TARGET $TARGET_OPTIONS
     echo "Valgrind completed. Check ./$LOG/valgrind.txt"
     exit 0
-else
-    echo "Usage: $0 [option]"
-    echo "Options:"
-    echo "  -c    Clean the project"
-    echo "  -b    Build the project"
-    echo "  -v    Test the project with Valgrind"
-    exit 1
 fi
